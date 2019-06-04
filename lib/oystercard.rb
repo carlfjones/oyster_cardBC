1d0132ded7a2a0e5300f60d4c759026974a425b5
Danielandro <mtme13@hotmail.com>
2019-06-04 16:57:35 +0100

    Add test for card limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,15 @@
 class Oystercard
-
+  LIMIT = 90
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
-  def top_up(amount)
+
+  def top_up(amount)    
+    raise "Max balance allowed is £#{LIMIT}" if (@balance + amount) > LIMIT       
     @balance += amount
+    
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,23 @@
 require 'oystercard'
 
-describe Oystercard do
+describe Oystercard do  
   it 'fresh card has a balance of 0' do
     expect(subject.balance).to eq(0)
   end
 
   describe '#top_up' do
-      it { is_expected.to respond_to(:top_up).with(1).argument }
+    limit = Oystercard::LIMIT
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'tops up balance with amount in argument' do
-      expect { subject.top_up(20) }.to change { subject.balance }.by (20)
+      card = Oystercard.new
+      card.top_up(10) 
+      expect(card.balance).to eq(10)
+      
+    end
+
+    it 'throws error if top up would exceed £#{limit}' do      
+      expect { subject.top_up(91) }.to raise_error("Max balance allowed is £#{limit}")
     end
   end
 

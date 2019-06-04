f1712507b8333aacf9be926b01671679fd2f2dc1
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-06-04 15:53:01 +0100

    Enabled top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,13 @@ describe Oystercard do
   it 'fresh card has a balance of 0' do
     expect(subject.balance).to eq(0)
   end
+
+  describe '#top_up' do
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up balance with amount in argument' do
+      expect { subject.top_up(20) }.to change { subject.balance }.by (20)
+    end
+  end
+
 end
